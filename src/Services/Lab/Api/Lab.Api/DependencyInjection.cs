444193026e51e6c17b1bba41666029507ee7000a
bintech-trungn <trungnguyen0803forwork@gmail.com>
2026-03-11 20:48:02 +0700

    hotfix: add cors and timeout duration

--- src/Services/Lab/Api/Lab.Api/DependencyInjection.cs
+++ src/Services/Lab/Api/Lab.Api/DependencyInjection.cs
@@ -23,17 +23,7 @@ public static class DependencyInjection
         // services.AddDistributedTracing(cfg);
         // services.AddSerilogLogging(cfg);
         services.AddCarter();
-        // CORS Configuration
-        services.AddCors(options =>
-        {
-            options.AddPolicy("AllowFrontend", policy =>
-            {
-                policy.WithOrigins("http://localhost:3000")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
-            });
-        });
+
         // HealthChecks
         {
             var dbType = cfg[$"{ConnectionStringsCfg.Section}:{ConnectionStringsCfg.DbType}"];
@@ -80,7 +70,6 @@ public static class DependencyInjection
                 ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
             });
 
-        app.UseCors("AllowFrontend");
         app.UseAuthentication();
         app.UseAuthorization();
         app.UseSwaggerApi();
