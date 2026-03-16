7de00c447097cd8779026ecf6f0865463f138d8b
bintech-trungn <trungnguyen0803forwork@gmail.com>
2026-03-16 22:26:00 +0700

    fix: fix redis cache key

--- src/Services/Management/Core/Management.Application/Features/Project/Queries/GetMyProjectRoleQuery.cs
+++ src/Services/Management/Core/Management.Application/Features/Project/Queries/GetMyProjectRoleQuery.cs
@@ -24,13 +24,13 @@ public sealed class GetMyProjectRoleQueryHandler(
     IRedisService redisService) : IQueryHandler<GetMyProjectRoleQuery, string>
 {
     public const string ProjectRolesKey = "ProjectRoles:";
-    public static string UserIdentifierKey(Guid userId) => $"{userId}";
+    public static string UserIdentifierKey(Guid userId) => $"{userId}:";
     
     #region Implementations
 
     public async Task<string> Handle(GetMyProjectRoleQuery req, CancellationToken cancellationToken)
     {
-        var cacheKey = $"{ProjectRolesKey}{req.UserId}:{req.ProjectId}";
+        var cacheKey = $"{ProjectRolesKey}{UserIdentifierKey(req.UserId)}{req.ProjectId}";
 
         var role = await redisService.GetOrSetCacheAsync(
             cacheKey,
