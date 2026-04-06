f1396dc092dbd7bbaa18f9f7887e6673a8b527d3
khanhhoang209 <lehoangkhanh209@gmail.com>
2026-04-07 00:59:49 +0700

    feat: add get section by id

---
+++ src/Services/Lab/Api/Lab.Api/Endpoints/GetSectionById.cs
@@ -0,0 +1,29 @@
+﻿using Lab.Api.Constants;
+using Lab.Application.Features.Section.Queries.GetSectionById;
+using Lab.Application.Models.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Api.Endpoints;
+
+public class GetSectionById : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet(ApiRoutes.Section.GetSectionById, HandleAsync)
+            .WithTags(ApiRoutes.Section.Tags)
+            .WithName(nameof(GetSectionById))
+            .Produces<ApiGetResponse<GetSectionByIdResult>>()
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+        // .RequireAuthorization();
+    }
+
+    private async Task<ApiGetResponse<GetSectionByIdResult>> HandleAsync(
+        ISender sender,
+        [FromRoute] Guid id)
+    {
+        var query = new GetSectionByIdQuery(id);
+        var result = await sender.Send(query);
+
+        return new ApiGetResponse<GetSectionByIdResult>(result);
+    }
+}
