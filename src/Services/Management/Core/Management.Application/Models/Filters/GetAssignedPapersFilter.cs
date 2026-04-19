cb3b03066b56345966d71688f5ddde27802b2536
Tri Quyet <nguyentriquyet010190@gmail.com>
2026-04-19 14:02:40 +0700

    fix: assign paper filter

--- src/Services/Management/Core/Management.Application/Models/Filters/GetAssignedPapersFilter.cs
+++ src/Services/Management/Core/Management.Application/Models/Filters/GetAssignedPapersFilter.cs
@@ -5,4 +5,5 @@ public sealed class GetAssignedPapersFilter
     public string? Title { get; set; }
     public string? ProjectName { get; set; }
     public string? ProjectCode { get; set; }
+    public Guid? ProjectId { get; set; }
 }
