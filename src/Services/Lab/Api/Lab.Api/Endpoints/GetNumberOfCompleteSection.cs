b145559187b07b0c8a00b7d2b1199659d0165011
Nguyễn Trí Quyết <156794243+quyetnt88@users.noreply.github.com>
2026-04-29 17:16:34 +0700

    feat: count number of complete section (#108)

---
+++ src/Services/Lab/Api/Lab.Api/Endpoints/GetNumberOfCompleteSection.cs
@@ -0,0 +1,27 @@
+﻿using Lab.Api.Constants;
+using Lab.Application.Features.Section.Queries.GetNumberOfCompleteSection;
+using Lab.Application.Models.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Api.Endpoints;
+
+public class GetNumberOfCompleteSection : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet(ApiRoutes.Section.GetNumberOfCompleteSection, GetNumberOfCompleteSectionHandler)
+            .WithTags(ApiRoutes.Section.Tags)
+            .WithName(nameof(GetNumberOfCompleteSection))
+            .Produces<ApiGetResponse<GetNumberOfCompleteSection>>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+
+    private async Task<ApiGetResponse<GetNumberOfCompleteSectionResult>> GetNumberOfCompleteSectionHandler(
+        ISender sender,
+        [FromRoute] Guid id)
+    {
+        var result = await sender.Send(new GetNumberOfCompleteSectionQuery(id));
+        return new ApiGetResponse<GetNumberOfCompleteSectionResult>(result);
+    }
+    
+}
